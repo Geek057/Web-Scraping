dd6ef9c01451d78c9f1743d0ac8b775e680a09ee
D_lip <dileepmeena57@gmail.com>
2017-09-10 14:47:57 +0530

    Modified FTP

--- FTP.py
+++ FTP.py
@@ -1,20 +1,40 @@
+from ftplib import FTP
 import os
-from ftplib import  FTP
-ftp = FTP('www.domainname.com')
-ftp.login('username','password')
-ftp.cwd('/public_html')
+f = FTP('www.domain.com')
+f.login('username','password')
+f.getwelcome()
+f.cwd('/public_html')
 data = []
-ftp.dir(data.append)
-[(str(i).split()[-1]) for i in data]
-lis = []
-for i in data:
-    lis.extend([(str(i).split()[-1]) for i in data])
+f.dir(data.append)
+filename = [str(i).split()[-1] for i in data]
+#(filename)
+
+def getfile(name):
+    opn = open(name,'wb')
+    f.retrbinary('RETR '+name,opn.write,1024)
+
+def sendfile():
+    name = input("ENTER FILE NAME: ")
+    if os.path.isfile(name):
+        f.storbinary('STOR '+name,open(name,'rb'))
+    else:
+        print("FIile not exits in current directory")
+
+os.chdir('/home/linux/Downloads')
+for i in filename:
+    print(i)
+
+print("Enter 1 for send file to server OR 2 to get file file from server")
+val = int(input())
+if val==1:
+    sendfile()
+elif val==2:
+    num = int(input(("Enter file number ")))
+    getfile(str(filename[num-1]))
+else:
+    print('Wrong command')
 
 
-def getfile():
-        for name in lis[3:]:
-            ftp.retrbinary('RETR '+name,open(name,'wb').write,1024)
-        ftp.quit()
 
 
-getfile()
+
